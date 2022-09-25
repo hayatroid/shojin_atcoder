74fe3e81aa7345e64da9f87c0ff0e488bec45a0e
hayatroid <hayatroid@gmail.com>
2022-09-25 20:34:37 +0900

    解いた！

--- abc270/src/bin/b.rs
+++ abc270/src/bin/b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    
+    input! {
+        mut x: i32,
+        mut y: i32,
+        mut z: i32,
+    }
+    if y < 0 {
+        x = -x;
+        y = -y;
+        z = -z;
+    }
+    let ans = if x < y {
+        x.abs()
+    } else if y < z {
+        -1
+    } else {
+        z.abs() + (x - z).abs()
+    };
+    println!("{}", ans);
 }
