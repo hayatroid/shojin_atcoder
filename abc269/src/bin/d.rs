ac3136e27949a8e2af5c7ff7719c6116c6145e4b
hayatroid <hayatroid@gmail.com>
2022-09-27 00:49:00 +0900

    解いた！

--- abc269/src/bin/d.rs
+++ abc269/src/bin/d.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    
+    input! {
+        n: usize,
+        xy: [(i32, i32); n],
+    }
+    let mut g = vec![vec!['.'; 2001]; 2001];
+    for (x, y) in xy {
+        let (x, y) = ((x + 1000) as usize, (y + 1000) as usize);
+        g[x][y] = '#';
+    }
+    let mut ans = 0;
+    for i in 0..=2000 {
+        for j in 0..=2000 {
+            if g[i][j] == '#' {
+                let mut dfs = vec![(i, j)];
+                while let Some((x, y)) = dfs.pop() {
+                    g[x][y] = '.';
+                    for (dx, dy) in vec![(!0, !0), (!0, 0), (0, !0), (0, 1), (1, 0), (1, 1)] {
+                        let (nx, ny) = (x + dx, y + dy);
+                        if nx <= 2000 && ny <= 2000 && g[nx][ny] == '#' {
+                            dfs.push((nx, ny));
+                        }
+                    }
+                }
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
