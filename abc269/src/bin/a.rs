484451963273ac0263b50524ae9d767e035ad0e5
hayatroid <hayatroid@gmail.com>
2022-09-26 23:46:27 +0900

    解いた！

--- abc269/src/bin/a.rs
+++ abc269/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    
+    input! {
+        a: i32,
+        b: i32,
+        c: i32,
+        d: i32,
+    }
+    println!("{}", (a + b) * (c - d));
+    println!("Takahashi");
 }
