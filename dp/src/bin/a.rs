b1b023166cebfd6c47aca76b7eec2b052f388029
hayatroid <hayatroid@gmail.com>
2022-09-27 01:14:21 +0900

    解いた！

--- dp/src/bin/a.rs
+++ dp/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    
+    input! {
+        n: usize,
+        h: [i32; n],
+    }
+    let mut dp = vec![1 << 29; n];
+    dp[0] = 0;
+    dp[1] = (h[1] - h[0]).abs();
+    for i in 2..n {
+        dp[i] = dp[i].min(dp[i - 1] + (h[i] - h[i - 1]).abs());
+        dp[i] = dp[i].min(dp[i - 2] + (h[i] - h[i - 2]).abs());
+    }
+    println!("{}", dp[n - 1]);
 }
