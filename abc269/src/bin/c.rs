c95b562664efbb73501e86641fedb2bd8ead144e
hayatroid <hayatroid@gmail.com>
2022-09-27 00:16:28 +0900

    解いた！

--- abc269/src/bin/c.rs
+++ abc269/src/bin/c.rs
@@ -1,3 +1,15 @@
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    
+    input! {
+        n: u64,
+    }
+    let mut ans = vec![n];
+    let mut i = n;
+    while i > 0 {
+        i = (i - 1) & n;
+        ans.push(i);
+    }
+    println!("{}", ans.iter().rev().join("\n"));
 }
