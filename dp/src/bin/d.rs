d8dd68aa6e3e579ee0853e94804a59f2e8fea40d
hayatroid <hayatroid@gmail.com>
2022-09-29 01:20:19 +0900

    解いた！

--- dp/src/bin/d.rs
+++ dp/src/bin/d.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    
+    input! {
+        n: usize,
+        w_lim: usize,
+        wv: [(usize, u64); n],
+    }
+    let mut dp = vec![0; w_lim + 1];
+    for (w, v) in wv {
+        for i in (w..=w_lim).rev() {
+            dp[i] = dp[i].max(dp[i - w] + v);
+        }
+    }
+    println!("{}", dp[w_lim]);
 }
