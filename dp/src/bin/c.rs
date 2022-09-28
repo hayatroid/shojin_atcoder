ca7f94fece9f69059364bcd6bc54d60190bcc631
hayatroid <hayatroid@gmail.com>
2022-09-29 01:11:27 +0900

    解いた！

--- dp/src/bin/c.rs
+++ dp/src/bin/c.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    
+    input! {
+        n: usize,
+        abc: [(u32, u32, u32); n],
+    }
+    let mut dp = vec![vec![0; 3]; n + 1];
+    for (i, &(a, b, c)) in abc.iter().enumerate() {
+        dp[i + 1][0] = dp[i][1].max(dp[i][2]) + a;
+        dp[i + 1][1] = dp[i][2].max(dp[i][0]) + b;
+        dp[i + 1][2] = dp[i][0].max(dp[i][1]) + c;
+    }
+    println!("{}", dp[n].iter().max().unwrap());
 }
